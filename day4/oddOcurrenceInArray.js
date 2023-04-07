3ed7c17368eee40fdd6a5dee4054d0743522b227
DavGonzaj <davgonzaj@gmail.com>
2023-04-07 08:59:14 -0700

    Algorithms

---
+++ day4/oddOcurrenceInArray.js
@@ -0,0 +1,47 @@
+// https://app.codility.com/programmers/lessons/2-arrays/odd_occurrences_in_array/
+
+/* 
+Given a non-empty array of odd length containing ints where every int but one
+has a matching pair (another int that is the same)
+return the only int that has no matching pair.
+*/
+
+const nums1 = [1];
+const expected1 = 1;
+
+const nums2 = [5, 4, 5];
+const expected2 = 4;
+
+const nums3 = [5, 4, 3, 4, 3, 4, 5];
+const expected3 = 4; // there is a pair of 4s but one 4 has no pair.
+
+const nums4 = [5, 2, 6, 2, 3, 1, 6, 3, 2, 5, 2];
+const expected4 = 1;
+
+function makeFrequencyTable(arr) {
+  const freqTable={}
+  for(let i = 0;i<arr.length;i++){
+    if(!freqTable[arr[i]]){
+      freqTable[arr[i]] = 1
+    // }else{
+    //   freqTable[arr[i]]++
+    }
+    // console.log(freqTable)
+  }
+  return freqTable
+}
+
+function oddOccurrencesInArray(nums) {
+    var newTable = makeFrequencyTable(nums)
+    for(const key in newTable){
+      if(newTable[key] % 2 == 1){
+        return key
+      }
+      console.log(newTable[key])
+    }
+}
+
+console.log(oddOccurrencesInArray(nums1), "should equal", expected1);
+console.log(oddOccurrencesInArray(nums2), "should equal", expected2);
+console.log(oddOccurrencesInArray(nums3), "should equal", expected3);
+console.log(oddOccurrencesInArray(nums4), "should equal", expected4);
