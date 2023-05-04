ca8a8b109a70b5d03acdb41b8e6001b226f80721
DavGonzaj <davgonzaj@gmail.com>
2023-05-04 09:05:17 -0700

    Algorithms

--- zMERN ALGOS/mergeSort.js
+++ zMERN ALGOS/mergeSort.js
@@ -1,24 +1,50 @@
+// merge
+const sortedA1 = [];
+const sortedB1 = [2];
+const expectedMerge1 = [2];
 
-// function merge(left = [], right = []) {
-//     let arr = []
-//     // Break out of loop if any one of the array gets empty
-//     while (left.length && right.length) {
-//         // Pick the smaller among the smallest element of left and right sub arrays 
-//         if (left[0] < right[0]) {
-//             arr.push(left.shift())  
-//         } else {
-//             arr.push(right.shift()) 
-//         }
-//     }
+const sortedA2 = [5];
+const sortedB2 = [2];
+const expectedMerge2 = [2, 5];
+
+const sortedA3 = [2, 3, 4, 7];
+const sortedB3 = [ 3];
+const expectedMerge3 = [2, 3, 3, 4, 7];
+
+const sortedA4 = [1, 2, 4, 5, 6, 9];
+const sortedB4 = [3, 7, 8, 10]
+const expectedMerge4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/**
+ * Efficiently merges two already sorted arrays into a new sorted array.
+ * Do not mutate the given arrays.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} left
+ * @param {Array<number>} right
+ * @returns {Array<number>} A new sorted array containing all the elements of
+ *    both given halves.
+ */
+function merge(left = [], right = []) {
+    let arr = []
+    // Break out of loop if any one of the array gets empty
+    while (left.length && right.length) {
+        // Pick the smaller among the smallest element of left and right sub arrays 
+        if (left[0] < right[0]) {
+            arr.push(left.shift())  
+        } else {
+            arr.push(right.shift()) 
+        }
+    }
     
-//     // Concatenating the leftover elements
-//     // (in case we didn't go through the entire left or right array)
-//     return [ ...arr, ...left, ...right ]
-// }
-// console.log(merge(sortedA1,sortedB1))
-// console.log(merge(sortedA2,sortedB2))
-// console.log(merge(sortedA3,sortedB3))
-// console.log(merge(sortedA4,sortedB4))
+    // Concatenating the leftover elements
+    // (in case we didn't go through the entire left or right array)
+    return [ ...arr, ...left, ...right ]
+}
+console.log(merge(sortedA1,sortedB1))
+console.log(merge(sortedA2,sortedB2))
+console.log(merge(sortedA3,sortedB3))
+console.log(merge(sortedA4,sortedB4))
 
 
 
