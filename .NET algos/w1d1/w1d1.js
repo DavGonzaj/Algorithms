82eae8d7352714135bd41390bfd80e4aa32abdc2
DavGonzaj <davgonzaj@gmail.com>
2023-06-08 08:44:28 -0700

    NEw algos

--- .NET algos/w1d1/w1d1.js
+++ .NET algos/w1d1/w1d1.js
@@ -40,7 +40,90 @@ class SinglyLinkedList {
     /** @type {ListNode|null} */
     this.head = null;
   }
+  /**
+   * Retrieves the data of the second to last node in this list.
+   * - Time: O(n) linear, n = length of list.
+   * - Space: O(1) constant.
+   * @returns {any} The data of the second to last node or null if there is no
+   *    second to last node.
+   */
+  secondToLast() {
+    if (this.isEmpty() || this.head.next === null) {
+      return null;
+    }
 
+    let runner = this.head;
+    while (runner.next.next) {
+      runner = runner.next;
+    }
+
+    return runner.data;
+  }
+
+  /**
+   * Removes the node that has the matching given val as its data.
+   * - Time: O(n) linear, n = length of list.
+   * - Space: O(1) constant.
+   * @param {any} val The value to compare to the node's data to find the
+   *    node to be removed.
+   * @returns {boolean} Indicates if a node was removed or not.
+   */
+  removeVal(val) {
+    if (this.isEmpty()) {
+      return false;
+    }
+
+    if (this.head.data === val) {
+      this.removeHead();
+      return true;
+    }
+
+    let runner = this.head;
+
+    while (runner.next) {
+      if (runner.next.data === val) {
+        runner.next = runner.next.next;
+        return true;
+      }
+      runner = runner.next;
+    }
+
+    return false;
+  }
+
+  /**
+   * Inserts a new node before a node that has the given value as its data.
+   * - Time: O(n) linear, n = length of list.
+   * - Space: O(1) constant.
+   * @param {any} newVal The value to use for the new node that is being added.
+   * @param {any} targetVal The value to use to find the node that the newVal
+   *    should be inserted in front of.
+   * @returns {boolean} To indicate whether the node was pre-pended or not.
+   */
+  prepend(newVal, targetVal) {
+    if (this.isEmpty()) {
+      return false;
+    }
+
+    if (this.head.data === targetVal) {
+      this.insertAtFront(newVal);
+      return true;
+    }
+
+    let runner = this.head;
+
+    while (runner.next) {
+      if (runner.next.data === targetVal) {
+        const newNode = new ListNode(newVal);
+        newNode.next = runner.next;
+        runner.next = newNode;
+        return true;
+      }
+      runner = runner.next;
+    }
+
+    return false;
+  }
   /*
    * Removes the last node of this list.
    * - Time: O(?).
@@ -114,7 +197,21 @@ class SinglyLinkedList {
    *    max integer as it's data.
    * @returns {?number} The max int or null if none.
    */
-  recursiveMax(runner = this.head, maxNode = this.head) {}
+  recursiveMax(runner = this.head, maxNode = this.head) {
+    if (this.head === null) {
+      return null;
+    }
+
+    if (runner === null) {
+      return maxNode.data;
+    }
+
+    if (runner.data > maxNode.data) {
+      maxNode = runner;
+    }
+
+    return this.recursiveMax(runner.next, maxNode);
+  }
 
   /**
    * Creates a new node with the given data and inserts that node at the front
@@ -291,6 +388,12 @@ console.log(unorderedList.removeHead().toArr());
 console.log(unorderedList.average());
 console.log(unorderedList.removeBack().toArr());
 console.log(unorderedList.contains());
+console.log(singleNodeList.secondToLast());
+console.log(unorderedList.secondToLast());
+console.log(singleNodeList.removeVal(1));
+console.log(unorderedList.removeVal(0));
+console.log(singleNodeList.prepend());
+console.log(unorderedList.prepend());
 
 /* node 4 connects to node 1, back to head */
 // const perfectLoopList = new SinglyLinkedList().insertAtBackMany([1, 2, 3, 4]);
