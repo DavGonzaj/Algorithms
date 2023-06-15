5abaf4434128c25ceb2538ca2692e8899650ed0e
DavGonzaj <davgonzaj@gmail.com>
2023-06-15 08:49:24 -0700

    New Algos

--- .NET algos/week 2 algos/binary_search.js
+++ .NET algos/week 2 algos/binary_search.js
@@ -38,6 +38,61 @@ class BinarySearchTree {
     this.root = null;
   }
   /**
+   * DFS Preorder: (CurrNode, Left, Right)
+   * Converts this BST into an array following Depth First Search preorder.
+   * Example on the fullTree var:
+   * [25, 15, 10, 4, 12, 22, 18, 24, 50, 35, 31, 44, 70, 66, 90]
+   * @param {Node} node The current node during the traversal of this tree.
+   * @param {Array<number>} vals The data that has been visited so far.
+   * @returns {Array<number>} The vals in DFS Preorder once all nodes visited.
+   */
+  toArrPreorder(node = this.root, vals = []) {
+    if (node) {
+      vals.push(node.data); // pushing current node
+      this.toArrPreorder(node.left, vals); // Traverse the left subtree
+      this.toArrPreorder(node.right, vals); // Traverse the right subtree
+    }
+    return vals;
+  }
+
+  /**
+   * DFS Inorder: (Left, CurrNode, Right)
+   * Converts this BST into an array following Depth First Search inorder.
+   * See debugger call stack to help understand the recursion.
+   * Example on the fullTree var:
+   * [4, 10, 12, 15, 18, 22, 24, 25, 31, 35, 44, 50, 66, 70, 90]
+   * @param {Node} node The current node during the traversal of this tree.
+   * @param {Array<number>} vals The data that has been visited so far.
+   * @returns {Array<number>} The vals in DFS Preorder once all nodes visited.
+   */
+  toArrInorder(node = this.root, vals = []) {
+    if (node) {
+      this.toArrInorder(node.left, vals); // Traverse the left subtree
+      vals.push(node.data); // pushing current node
+      this.toArrInorder(node.right, vals); // Traverse the right subtree
+    }
+    return vals;
+  }
+
+  /**
+   * DFS Postorder (Left, Right, CurrNode)
+   * Converts this BST into an array following Depth First Search postorder.
+   * Example on the fullTree var:
+   * [4, 12, 10, 18, 24, 22, 15, 31, 44, 35, 66, 90, 70, 50, 25]
+   * @param {Node} node The current node during the traversal of this tree.
+   * @param {Array<number>} vals The data that has been visited so far.
+   * @returns {Array<number>} The vals in DFS Preorder once all nodes visited.
+   */
+  toArrPostorder(node = this.root, vals = []) {
+    if (node) {
+      this.toArrPostorder(node.left, vals); // Traverse the left subtree
+      this.toArrPostorder(node.right, vals); // Traverse the right subtree
+      vals.push(node.data); /// pushing current node
+    }
+    return vals;
+  }
+
+  /**
    * Inserts a new node with the given newVal in the right place to preserver
    * the order of this tree.
    * - Time: O(?).
@@ -365,4 +420,11 @@ fullTree
   .insert(66)
   .insert(90);
 
-fullTree.print();
+// fullTree.print();
+
+console.log(fullTree.toArrPreorder());
+console.log(fullTree.toArrInorder());
+console.log(fullTree.toArrPostorder());
+console.log(emptyTree.toArrPreorder());
+console.log(emptyTree.toArrInorder());
+console.log(emptyTree.toArrPostorder());
